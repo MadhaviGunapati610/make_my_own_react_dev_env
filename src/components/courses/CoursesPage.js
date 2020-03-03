b86d8219bde44e55b5ea13d7e120dd19500d33c9
madhavi <gmadhavi610@gmail.com>
2020-03-03 10:19:20 -0500

    Pushung basic setup of an application

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class CoursesPage extends React.Component{
+    state={
+        course: {
+            title:""
+        }
+    }
+    handleChange = (e) => {
+        const course = {...this.state.course, title: e.target.value}
+        this.setState({course})
+    }
+
+    handleSubmit=(e)=> {
+        e.preventDefault();
+        alert(`${this.state.course.title}, hi`);
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+            <h2>Courses Page</h2>
+            <h3>Add Course</h3>
+            <input type="text" value={this.state.course.title} onChange={this.handleChange}/>
+            <input type="submit" value="Save" />
+            </form>
+        )
+    }
+}
+
+export default CoursesPage;
